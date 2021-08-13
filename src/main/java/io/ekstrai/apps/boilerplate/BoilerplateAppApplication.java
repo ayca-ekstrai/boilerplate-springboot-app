62ca24bc8a8646ce128c3625b958e11f9b4f800c
Ayca Kucukdurmaz <ayca@Aycas-MacBook-Pro.local>
2021-08-13 16:41:55 +0300

    initial commit

---
+++ src/main/java/io/ekstrai/apps/boilerplate/BoilerplateAppApplication.java
@@ -0,0 +1,13 @@
+package io.ekstrai.apps.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ekstrai/apps/boilerplate/BoilerplateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ekstrai.apps.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
